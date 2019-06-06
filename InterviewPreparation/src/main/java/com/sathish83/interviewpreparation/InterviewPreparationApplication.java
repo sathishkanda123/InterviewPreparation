627cbc35a79376583b294b99f141435a7b236f77
sathish <sathish@DESKTOP-RQCTA2P>
2019-06-06 13:25:26 +0530

    Init commit project started

---
+++ InterviewPreparation/src/main/java/com/sathish83/interviewpreparation/InterviewPreparationApplication.java
@@ -0,0 +1,13 @@
+package com.sathish83.interviewpreparation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPreparationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewPreparationApplication.class, args);
+	}
+
+}
